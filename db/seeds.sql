f4c92c84767861025460f92715753ac0e973b0d5
Tim <timothy.mace@outlook.com>
2018-05-04 07:56:45 -0400

    initial commit with folder structure

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    burgers (burger_name, devoured)
+VALUES
+    ("Plain Jane", false),
+    ("The All-American", true),
+    ("Big 'n Cheesy Never Easy", false),
+    ("Mystery Meat Burger", false),
+    ("Grilled Patty Melt", true);
