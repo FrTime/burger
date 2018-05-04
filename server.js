a80ff4aaa51062cb0219fd579923922199c0b98e
Tim <timothy.mace@outlook.com>
2018-05-04 08:12:32 -0400

    added server code and .gitignore

---
+++ server.js
@@ -0,0 +1,22 @@
+// Setting the needed requirements and variables
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  PORT = process.env.PORT || 8080,
+  app = express(),
+  exphbs = require("express-handlebars"),
+  routes = require("./controllers/burgersController.js");
+
+// Setting the 'public' folder as a static directory to serve to the app
+app.use(express.static("public"));
+
+// Setting up body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Setting routes
+app.use(routes);
+
+// Starting the server on localhost:8080
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
